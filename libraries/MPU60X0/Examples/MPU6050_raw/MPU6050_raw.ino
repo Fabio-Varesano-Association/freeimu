7ebf1a8b4469a5ea32a472261e71138284aed512
Fabio Varesano <fvaresano@yahoo.it>
2012-12-12 13:16:21 +0100

    Making MPU6050_raw.ino example compatible with the new MPU60X0 class naming

--- libraries/MPU60X0/Examples/MPU6050_raw/MPU6050_raw.ino
+++ libraries/MPU60X0/Examples/MPU6050_raw/MPU6050_raw.ino
@@ -32,17 +32,18 @@ THE SOFTWARE.
 // Arduino Wire library is required if I2Cdev I2CDEV_ARDUINO_WIRE implementation
 // is used in I2Cdev.h
 #include "Wire.h"
+#include "SPI.h"
 
 // I2Cdev and MPU6050 must be installed as libraries, or else the .cpp/.h files
 // for both classes must be in the include path of your project
 #include "I2Cdev.h"
-#include "MPU6050.h"
+#include "MPU60X0.h"
 
 // class default I2C address is 0x68
 // specific I2C addresses may be passed as a parameter here
 // AD0 low = 0x68 (default for InvenSense evaluation board)
 // AD0 high = 0x69
-MPU6050 accelgyro;
+MPU60X0 accelgyro;
 
 int16_t ax, ay, az;
 int16_t gx, gy, gz;
